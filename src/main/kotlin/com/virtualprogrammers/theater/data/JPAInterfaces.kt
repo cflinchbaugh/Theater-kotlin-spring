a9250cf21e26e36da050cedeb686bb8f040ca8b8
Christopher Flinchbaugh <chris.f@Very-MacBook-Pro.local>
2021-11-08 10:58:38 -0500

    Implement ADD Performance functionality including DB Table standup

--- src/main/kotlin/com/virtualprogrammers/theater/data/JPAInterfaces.kt
+++ src/main/kotlin/com/virtualprogrammers/theater/data/JPAInterfaces.kt
@@ -1,6 +1,8 @@
 package com.virtualprogrammers.theater.data
 
+import com.virtualprogrammers.theater.domain.Performance
 import com.virtualprogrammers.theater.domain.Seat
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface SeatRepository : JpaRepository<Seat, Long>
+interface PerformanceRepository : JpaRepository<Performance, Long>
---
+++ src/main/kotlin/com/virtualprogrammers/theater/domain/Performance.kt
@@ -0,0 +1,13 @@
+package com.virtualprogrammers.theater.domain
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+data class Performance(@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+                val id: Long,
+                val title: String) {
+    override fun toString(): String = "Performance $id-$title"
+}
